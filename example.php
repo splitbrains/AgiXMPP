37a1cae8589b91bc45b7e7ada2bcf35870ad6f67
Daniel <Daniellehr@gmx.de>
2011-12-30 17:42:18 +0100

    deleted old, unnecessary comment

--- example.php
+++ example.php
@@ -7,23 +7,3 @@ $xmpp = new XMPP\Client($config);
 $xmpp->connect();
 
 $xmpp->main();
-
-//$xmpp->addEvent(function($xml) {}, );
-
-//do {
-  //$xmpp->waitForEvent(XMPP::HANDSHAKE);
-
-  /*
-
-  switch($xmpp->getEvent()) {
-    case 'connection':
-      echo 'successfully connected';
-
-      $xmpp->send();
-      break;
-  }*/
-
-
-//} while($xmpp->isConnected());
-
-//$xmpp->disconnect();
