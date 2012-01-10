d68488fc51b22db9fad1d612855f0042e17cee9d
Daniel <Daniellehr@gmx.de>
2012-01-10 17:59:29 +0100

    ..

--- example.php
+++ example.php
@@ -8,20 +8,7 @@ $config = require_once (!file_exists('config.mine.php') ? 'config.php' : 'config
 $xmpp = new XMPP\Client($config);
 $xmpp->connect();
 
-$xmpp->addEventHandlers(array('sessionStarted'));
-
 $xmpp->init();
 
-class Connected implements EventReceiver
-{
-  /**
-   * @param $eventName
-   * @param $that \XMPP\EventHandlers\EventObject
-   */
-  public function onEvent($eventName, $that)
-  {
-
-  }
-}
 
 $xmpp->disconnect();
