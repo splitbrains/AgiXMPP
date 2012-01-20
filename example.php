9fc84b917396a0ea302ddacd66a2ef46ad239432
Daniel <Daniellehr@gmx.de>
2012-01-20 17:33:01 +0100

    .

--- example.php
+++ example.php
@@ -5,6 +5,7 @@ $config = require_once (!file_exists('config.mine.php') ? 'config.php' : 'config
 
 $xmpp = new XMPP\Client($config);
 
+
 // add your awesome event handlers somewhere here
 if (!$xmpp->connect()) die();
 
