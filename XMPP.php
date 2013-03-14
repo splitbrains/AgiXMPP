449d4ccc72ee3a43a437d200b37b51a1b414ba24
Dan Lee <Daniellehr@gmx.de>
2013-03-14 17:27:05 +0100

    removed autoloader, not necessary

--- XMPP.php
+++ XMPP.php
@@ -9,18 +9,6 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', true);
 
 define('SRC_PATH', __DIR__ . '/src');
-define('NS', __NAMESPACE__);
-
-
-spl_autoload_register(function($class) {
-  // remove the leading namespace
-  if (substr($class, 0, strlen(NS)) == NS) {
-    $class = substr($class, strlen(NS), strlen($class));
-  }
-  $file = SRC_PATH . str_replace('\\', '/', $class) . '.php';
-
-  require_once $file;
-});
 
 $dir = new RecursiveDirectoryIterator(SRC_PATH, FilesystemIterator::CURRENT_AS_FILEINFO);
 $it  = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
