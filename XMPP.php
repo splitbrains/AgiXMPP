936a9aed367ef26115920a99c33c6443b27b264a
Dan Lee <Daniellehr@gmx.de>
2013-03-14 21:29:52 +0100

    re-added autoloader as it is needed however

--- XMPP.php
+++ XMPP.php
@@ -8,7 +8,16 @@ use RecursiveIteratorIterator;
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', true);
 
-define('SRC_PATH', __DIR__ . '/src');
+define('DS', DIRECTORY_SEPARATOR);
+define('SRC_PATH', __DIR__ .DS .'src');
+
+spl_autoload_register(function($class) {
+  // remove the leading namespace
+  $class = substr($class, strlen(__NAMESPACE__), strlen($class));
+  $file = SRC_PATH .str_replace('\\', DS, $class).'.php';
+
+  require_once $file;
+});
 
 $dir = new RecursiveDirectoryIterator(SRC_PATH, FilesystemIterator::CURRENT_AS_FILEINFO);
 $it  = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
